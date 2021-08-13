05f3bce224f9b415998aa4726ac9a946233bdfac
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-13 22:29:55 +0530

    Refactored:Restricted E-mail must contain @ and yahoo, gamil, bridgelabz, anycharacter and invalid special character after @

--- validateEmail.java
+++ validateEmail.java
@@ -9,7 +9,7 @@ public class validateEmail {
 		System.out.println("Enter your E-mail: ");
 	    String email = new Scanner(System.in).next();
 	     
-	    String regex = "^(.+)@(.+)$";
+	    String regex = "^[A-Za-z0-9+_.-]+@+[A-Za-z]+$";
 	     
 	    Pattern pattern = Pattern.compile(regex);    
 	    Matcher matcher = pattern.matcher(email); 
