e72095acf317df82d4601b5e66996052b7c01b9c
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-13 22:43:58 +0530

    Refactored:Restricted email must contain .co/com/co.in/com.in

--- validateEmail.java
+++ validateEmail.java
@@ -9,7 +9,7 @@ public class validateEmail {
 		System.out.println("Enter your E-mail: ");
 	    String email = new Scanner(System.in).next();
 	     
-	    String regex = "^[A-Za-z0-9+_.-]+@+[A-Za-z]+$";
+	    String regex = "^[A-Za-z0-9+_.-]+@+(?:[a-zA-Z]+\\.)+[a-zA-Z]{2,6}$";
 	     
 	    Pattern pattern = Pattern.compile(regex);    
 	    Matcher matcher = pattern.matcher(email); 
